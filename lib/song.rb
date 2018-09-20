440fad57ab87b9d7a55d49a35aa8cf00afb78510
OGonzalezNYC <OGonzalezNYC@Gmail.com>
2018-09-20 01:01:17 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,16 @@
 
 require 'pry'
+
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+
   def self.all
     @@all
   end
+
 
   def save
     self.class.all << self
@@ -20,17 +24,9 @@ class Song
   end
 
 
-  # def self.create(name)
-  #     person = self.new
-  #     person.name = name
-  #     @@all << person
-  #   end
-
-
   def self.new_by_name(name)
     song = self.new
     song.name = name
-#    @artist_name = artist_name
     song
   end
 
@@ -46,18 +42,12 @@ class Song
   def self.find_by_name(name)
     found_song = nil
     @@all.map do |song|
-#  binding.pry
       if song.name == name
         found_song = song
       end
     end
     found_song
   end
-
-
-# In order to prevent duplicate songs being created that actually represent the same song (based on the song name),
-#we're going to build a Song.find_or_create_by_name class method. This method will accept a string name for a song and either
-#return a matching song instance with that name or create a new song with the name and return the song instance.
 
 
   def self.find_or_create_by_name(name)
@@ -69,45 +59,31 @@ class Song
 
 
    def self.alphabetical
-
+     @@all.sort_by do |song|
+       song.name
+     end
    end
-  #
-   #<Song:0x00007f9854c27b00 @name="Blank Space">
-   # def self.new_from_file_name(mp3_filename)
-   #   song = self.new
-   #   song.
-   # end
-  #
-  #'abc123'.chomp('123')    # => "abc"
-#   song = Song.new_from_filename("Taylor Swift - Blank Space.mp3")
-# song.name #=> "Blank Space"
-# song.artist_name #=> "Taylor Swift"
+
+
    def self.new_from_filename(mp3_name)
      mp3_name_array = mp3_name.split(" - ")
-
-     song_name = mp3_name_array[1].chomp(".mp3")
-  #   binding.pry
-  #   new_by_name(@name)
+     name = mp3_name_array[1].chomp(".mp3")
+    # song_name = mp3_name_array[1].chomp(".mp3")
      artist_name = mp3_name_array[0]
      song = self.new
-     song.name = song_name
+     song.name = name
+  #   song.name = song_name
      song.artist_name = artist_name
      song
-  #   song.artist_name =
-  #   @name.artist_name = @artist_name
-  #   song = self.new
-  #    song.name = @name
-  #   song.artist_name = @artist_name
    end
 
 
-   def create_from_file_name
-
+   def self.create_from_filename(mp3_name)
+     @@all << new_from_filename(mp3_name)
    end
 
 
   def self.destroy_all
-  #  @@all.clear
     self.all.clear
   end
 
